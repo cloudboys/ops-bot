d8ee3cd6ae666c062efb310092547d4143e61f3d
kondapallivjagan <kondapallivjagan@users.noreply.github.com>
2017-12-23 21:44:36 -0600

    Update splunk_alert_action.sh

--- splunk_alert_action.sh
+++ splunk_alert_action.sh
@@ -6,7 +6,7 @@ UUID=$(cat /proc/sys/kernel/random/uuid);echo $UUID;gunzip -c $8 > /tmp/$UUID.cs
 # Now Clone the git every single time perform necessary task. - Logic is left in git to acheive scalability
 cd /tmp/
 echo "Cloning latest code from git"
-env GIT_SSL_NO_VERIFY=true git clone -b master http://5d6571f550cfa75350de2424ea4110bad349ce3b:x-oauth-basic@github.homeawaycorp.com/DBOPS/nosql-bot.git
+env GIT_SSL_NO_VERIFY=true git clone -b master http://5d6571f550cfa75350de2424ea4110bad349ce3b:x-oauth-basic@github.com/cloudboys/nosql-bot.git
 chmod +x /tmp/nosql-bot/splunk_alert_action.py
 
 # Covnvert the csv to json and push the message to SQS
